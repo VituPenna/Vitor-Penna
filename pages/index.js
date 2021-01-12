4c28b44e090200b185964981c9f1b0e87073eba7
snaipasan <62647884+snaipasan@users.noreply.github.com>
2021-01-12 12:12:58 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Vitor Penna</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Hi, I‘m Vitor Penna, a Brazilian fullstack developer!</h1>
+
+        <div className={styles.grid}>
+          
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+       
+      </footer>
+    </div>
+  )
+}
